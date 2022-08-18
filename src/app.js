5c0a90098d7c6064c51a350967f75413667f7905
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-08-18 04:58:09 -0700

    feat: :sparkles: adding cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 
 const appointmentsRouter = require('./routes/appointments.routes')
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json({ limit: '10kb' }))
 
